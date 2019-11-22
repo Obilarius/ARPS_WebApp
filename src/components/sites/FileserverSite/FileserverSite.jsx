4ebe6b1b87cbffbf13a519dde773e6d47877cac8
Obilarius <git@walzenbach.com>
2019-11-22 15:30:56 +0100

    InfoTiles finished

--- src/components/sites/FileserverSite/FileserverSite.jsx
+++ src/components/sites/FileserverSite/FileserverSite.jsx
@@ -1,15 +1,54 @@
 import React, { Component } from "react";
+import axios from "axios";
 import "./FileserverSite.scss";
 import Tiles from "./Tiles";
 
 export default class FileserverSite extends Component {
+  state = {
+    fileserver: [],
+    distinctFileserver: [],
+    folderCount: 0,
+    sumSize: 0
+  };
+
+  distinctFileserver = () => {
+    const fs = this.state.fileserver;
+    const dfs = [];
+
+    fs.forEach(server => {
+      const name = server._unc_path_name.substring(2).split("\\")[0];
+      if (dfs.indexOf(name) === -1) dfs.push(name);
+    });
+
+    this.setState({ distinctFileserver: dfs });
+  };
+
+  componentDidMount = () => {
+    axios.get("http://localhost:8000/fileserver/shares").then(res => {
+      this.setState({ fileserver: res.data.recordset });
+      this.distinctFileserver();
+    });
+
+    axios.get("http://localhost:8000/fileserver/foldercount").then(res => {
+      this.setState({ folderCount: res.data.recordset[0].folderCount });
+    });
+
+    axios.get("http://localhost:8000/fileserver/sumsize").then(res => {
+      this.setState({ sumSize: res.data.recordset[0].sum });
+    });
+  };
+
   render() {
     return (
       <div id="_ea08ff" className="container">
         <div className="header">
           <h2>Fileserver</h2>
         </div>
-        <Tiles />
+        <Tiles
+          distinctFileserver={this.state.distinctFileserver.length}
+          folderCount={this.state.folderCount}
+          sumSize={this.state.sumSize}
+        />
       </div>
     );
   }
--- src/components/sites/FileserverSite/Tiles.jsx
+++ src/components/sites/FileserverSite/Tiles.jsx
@@ -4,18 +4,46 @@ import { ReactComponent as FolderSolid } from "../../../assets/FontAwesome/folde
 import { ReactComponent as ServerSolid } from "../../../assets/FontAwesome/server-solid.svg";
 import { ReactComponent as HDDSolid } from "../../../assets/FontAwesome/hdd-solid.svg";
 
-const Tiles = () => {
+const Tiles = props => {
+  const numberWithCommas = x => {
+    if (x) return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+  };
+
+  const humanFileSize = (bytes, si) => {
+    var thresh = 1024;
+    if (Math.abs(bytes) < thresh) {
+      return bytes + " B";
+    }
+    var units = ["kB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
+    var u = -1;
+    do {
+      bytes /= thresh;
+      ++u;
+    } while (Math.abs(bytes) >= thresh && u < units.length - 1);
+    // return bytes.toFixed(1) + " " + units[u];
+    return (
+      <>
+        {bytes.toFixed(1)}
+        <span>{units[u]}</span>
+      </>
+    );
+  };
+
   return (
     <div className="tiles">
-      <Tile icon={<ServerSolid />} number="4" text="verschiedene Fileserver" />
-      <Tile icon={<FolderSolid />} number="5210" text="Ordner auf Fileserver" />
+      <Tile
+        icon={<ServerSolid />}
+        number={props.distinctFileserver}
+        text="verschiedene Fileserver"
+      />
+      <Tile
+        icon={<FolderSolid />}
+        number={numberWithCommas(props.folderCount)}
+        text="Ordner auf Fileserver"
+      />
       <Tile
         icon={<HDDSolid />}
-        number={
-          <>
-            30<span>TB</span>
-          </>
-        }
+        number={<>{humanFileSize(props.sumSize, true)}</>}
         text="belegter Speicherplatz"
       />
     </div>
